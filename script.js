8fcc0b30017512fdf53f95606b2ba018c4c08a95
alfianardikha <alfianardikhax4@gmail.com>
2025-10-16 13:37:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-const tombolsapa = document.getElementById('sapaButton');
-tombolSapa.addEventListener('click',function() {
-alert('halo! terima kasih sudah berkunjung!');});
+const tombolSapa = document.getElementById('sapaButton');
+tombolSapa.addEventListener('click', function() {
+  alert('Halo! Terima kasih sudah berkunjung!');
+});
